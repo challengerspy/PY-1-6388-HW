178f6e6efddcd8930e55a4f6ca205ec32a87c641
challengerspy <48886351+challengerspy@users.noreply.github.com>
2019-04-07 12:54:23 +0300

    Update hw02_easy.py

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -13,13 +13,48 @@
 
 # Подсказка: воспользоваться методом .format()
 
+spisok=[]
+spisok=['яблоко','банан','киви','арбуз']
+print(spisok)
+i=0
+y=1
+while i < len (spisok):
+    print(f'{y}. ' + '{:>6}'.format(spisok[i]))
+    i+=1
+    y+=1
 
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
 
+spisok1=list()
+spisok2=list()
+spisok1=['яблоко','банан','киви','арбуз']
+spisok2=['апельсин','лимон','киви','арбуз']
+spisok3=[]
+
+for i in spisok1:
+    for x in spisok2:
+        if x in i:
+            spisok1.append('1')
+            spisok1.remove(i)
+
+print(spisok1)
+print(spisok2)
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+
+spisok1=[]
+spisok1=[4,2,6,8,88,100,67]
+spisok2=[]
+i=0
+while i < len(spisok1):
+    if int(spisok1[i]) % 2 == 0:
+        spisok2.append(int(spisok1[i]/4))
+    if int(spisok1[i] % 2 != 0):
+        spisok2.append(int(spisok1[i])*2)
+    i+=1
+print(spisok2)
